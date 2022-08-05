b2d2dd8b35d2efa97c49f5a1274ebe60760b64f1
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-05 15:09:57 +0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './layout/SharedLayout';
+import { GlobalStyle } from './styleConfig/GlobalStyle';
 
 const HomePage = lazy(() => import('./pages/HomePage'));
 const BreedsPage = lazy(() => import('./pages/BreedsPage'));
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />}></Route>
-        <Route path="/breeds" element={<BreedsPage />}></Route>
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/breeds" element={<BreedsPage />}></Route>
+        </Route>
+      </Routes>
+      <GlobalStyle />
+    </>
   );
 };
 
